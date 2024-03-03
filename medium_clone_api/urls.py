237a03ead90810684fe5ac1808d7355e8400e54e
Maxim Manzik <kepkpkep@gmail.com>
2024-03-03 16:14:49 +0200

    setup nginx

--- medium_clone_api/urls.py
+++ medium_clone_api/urls.py
@@ -18,7 +18,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
