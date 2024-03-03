0892e1af430f3541f922465a075d2df1f3029167
Maxim Manzik <kepkpkep@gmail.com>
2024-03-03 22:37:33 +0200

    profile model and signal create

--- core_apps/profiles/admin.py
+++ core_apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['pkid', 'id', 'user', 'gender', 'phone_number', 'country', 'city']
+    list_display_links = ['pkid', 'id', 'user']
+    list_filter = ['id', 'pkid']
